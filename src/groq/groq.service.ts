4901fb2a5ba8660891b1cacc7e87850bba2c4ad5
Septian Maulana <septiandev028@gmail.com>
2024-09-01 09:23:05 +0700

    feat(ai): update system persona using created friend from database

--- src/groq/groq.service.ts
+++ src/groq/groq.service.ts
@@ -2,12 +2,16 @@ import { BadGatewayException, Injectable } from '@nestjs/common';
 import { ChatGroq } from '@langchain/groq';
 
 import { SharedService } from '../shared/shared.service';
+import { FriendsService } from 'src/friends/friends.service';
 
 @Injectable()
 export class GroqService {
   private chatGroq: ChatGroq;
 
-  constructor(private readonly sharedService: SharedService) {
+  constructor(
+    private readonly sharedService: SharedService,
+    private readonly friendsService: FriendsService,
+  ) {
     this.chatGroq = new ChatGroq({
       model: 'llama-3.1-8b-instant',
       temperature: 0,
@@ -44,15 +48,27 @@ export class GroqService {
   //   }
   // }
 
-  async handleMessage(message: string): Promise<any> {
+  async handleMessage(friendId: string, message: string): Promise<any> {
     try {
+      const friend = await this.friendsService.findFriendById(
+        parseInt(friendId),
+      );
+
       const resultFromLangchain = await this.chatGroq.invoke([
         [
           'system',
-          'You are a helpful assistant that translates English to Indonesia. Translate the user sentence.',
+          `
+          You are ${friend.name}, a ${friend.age}-year-old ${friend.gender.toLowerCase()} ${friend.occupation} based in ${friend.location}.
+          ${friend.shortBio}
+          You have a ${friend.personality.slice(0, -1).join(', ')}, and ${friend.personality[friend.interests.length - 1]}.
+          Your interests include ${friend.interests.slice(0, -1).join(', ')}, and ${friend.interests[friend.interests.length - 1]}.
+          Your role is to provide empathetic and insightful support, offering advice and strategies rooted in your extensive experience as a ${friend.occupation}.
+          Please respond as ${friend.name} in all interactions, using your expertise and personality traits to assist in any situation.
+          `,
         ],
         ['human', message],
       ]);
+
       return resultFromLangchain.content;
 
       // const resultDirectFromGroq = await this.generate(message);
